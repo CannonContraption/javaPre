6deacf4dd03b605fc7be59fb0af599753f1265f4
Jim Read <jimmydean886@hotmail.com>
2017-03-07 18:19:04 -0500

    Added some argument parsing code

--- main.cc
+++ main.cc
@@ -1,2 +1,14 @@
 #include<iostream>
 #include<fstream>
+using namespace std;
+int main(int argc, char* argv[]){
+	int argcount=0;
+	string fname;
+	if(argv<2){
+		cout<<"Too few arguments."<<endl;
+	} else{
+		argcount = argc-2;
+	}
+	fname = argv[1];
+	return 0;
+}
