e6eac3cb06037fe836a5bcdf5d869de235d5dcc9
Jim Read <jimmydean886@hotmail.com>
2017-03-07 22:20:17 -0500

    Split kv class from main.cc
    Also added build gitignore

--- main.cc
+++ main.cc
@@ -4,69 +4,7 @@
 #define VOWEL 4
 #define CONSONANT 1
 using namespace std;
-
-class kv{
-public:
-	struct pair{
-		string key;
-		string value;
-		pair* next;
-		pair* prev;
-	};
-	pair * listofpairs[HASHSIZE];
-	char vow[12] = {'a','e','i','o','u','y','A','E','I','O','U','Y'};
-	int hash(string key){
-		int sum = 0;
-		bool got = false;
-		for(char c : key){
-			for(char d : vow){
-				if(c == d){
-					sum+=VOWEL;
-					got = true;
-				}
-			}
-			if(!got) sum+=CONSONANT;
-			got=false;
-		}
-		return sum%HASHSIZE;
-	}
-	void insert(string key, string val){
-		int hashindex = hash(key);
-		if(listofpairs[hashindex] == NULL){
-			listofpairs[hashindex] = new pair;
-			listofpairs[hashindex]->key=key;
-			listofpairs[hashindex]->value=val;
-			listofpairs[hashindex]->next=nullptr;
-			listofpairs[hashindex]->prev=nullptr;
-		} else {
-			pair* current = listofpairs[hashindex];
-			while(current->next != nullptr){
-				current = current->next;
-			}
-			current -> next = new pair;
-			current -> next -> prev = current;
-			current = current -> next;
-			current -> key = key;
-			current -> value = val;
-		}
-	}
-	string grab(string key){
-		int hashindex = hash(key);
-		if(listofpairs[hashindex]->key == key){
-			return listofpairs[hashindex]->value;
-		} else {
-			pair* current = listofpairs[hashindex];
-			while(current->key != key && current->next != nullptr){
-				current = current->next;
-			}
-			if(current->key == key){
-				return current->value;
-			} else {
-				return "";
-			}
-		}
-	}
-};
+#include "kv.cc"
 
 int main(int argc, char* argv[]){
 	int argcount=0;
